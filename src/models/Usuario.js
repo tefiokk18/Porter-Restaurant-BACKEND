af304a4b885076adac0604d3a9767d3dae6fa624
Stephania <s.juarez.dev@gmail.com>
2026-03-28 13:49:50 -0300

    agregue rol y campo activo para suspender usuarios

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -3,9 +3,10 @@ import mongoose from 'mongoose';
 const usuarioSchema = new mongoose.Schema({
     nombre: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    telefono: {type: String, required: true},
+    telefono: { type: String, required: true },
     password: { type: String, required: true },
-    rol: { type: String, default: 'usuario' }
+    rol: { type: String, default: 'usuario' }, 
+    activo: { type: Boolean, default: true } 
 });
 
 export default mongoose.model('Usuario', usuarioSchema);
