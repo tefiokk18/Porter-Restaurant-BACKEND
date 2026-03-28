fcc63a263e655f9ec504b60681074df30bf08a50
Stephania <s.juarez.dev@gmail.com>
2026-03-28 14:09:20 -0300

    modifique las rutas del usuario

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
-import { registrarUsuario, loginUsuario } from '../controllers/usuarios.controllers.js';
+import { 
+    registrarUsuario, 
+    loginUsuario, 
+    obtenerUsuarios,   
+    suspenderUsuario,   
+    eliminarUsuario     
+} from '../controllers/usuarios.controllers.js';
+import validarJWT from '../middlewares/validarJWT.js'; 
 
 const router = Router();
 
 router.post('/register', registrarUsuario);
-
 router.post('/login', loginUsuario);
+
+router.get('/usuarios', [validarJWT], obtenerUsuarios);
+router.put('/usuarios/suspender/:id', [validarJWT], suspenderUsuario);
+router.delete('/usuarios/eliminar/:id', [validarJWT], eliminarUsuario);
 
 export default router;
