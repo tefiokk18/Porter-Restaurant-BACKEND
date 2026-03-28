16c057ce18d8ffad17ef6869b43725700d5ea1a6
Luciana Roldan <lucianaroldan855@gmail.com>
2026-03-28 16:44:51 -0300

    Rescate de package.json e integración de seguridad en reservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,14 +1,15 @@
 import { Router } from 'express';
-import { crearReserva, obtenerReservas, editarReserva, eliminarReserva} from '../controllers/reservas.controllers.js';
+import { crearReserva, obtenerReservas, editarReserva, eliminarReserva } from '../controllers/reservas.controllers.js';
+import validarJWT from '../middlewares/validarJWT.js';
 
 const router = Router();
 
 router.route('/reservas')
-    .get(obtenerReservas)
+    .get([validarJWT], obtenerReservas)
     .post(crearReserva);
 
 router.route('/reservas/:id')
-    .put(editarReserva)
-    .delete(eliminarReserva); 
+    .put([validarJWT], editarReserva)
+    .delete([validarJWT], eliminarReserva);
 
 export default router;
